18f1b53d015bb115b9472c20633d78988076b369
angelo98-dev <angeyannickmien@gmail.com>
2020-09-06 15:35:29 +0100

    Agent Filtrateur

---
+++ src/Agents/AgentFiltreur.java
@@ -0,0 +1,15 @@
+package Agents;
+
+import jade.core.Agent;
+
+/**
+ * agent chargé de filtrer les donnees de description
+ */
+public class AgentFiltreur extends Agent {
+
+
+    protected void setup() {
+        System.out.println("Agent Filtreur : " + getLocalName() + " lancé");
+
+    }
+}
