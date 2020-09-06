63d30ba24c89d5a513d1059d2d243611a6f3b188
angelo98-dev <angeyannickmien@gmail.com>
2020-09-06 15:36:22 +0100

    Agent Stockage

--- src/Agents/AgentStockage.java
+++ src/Agents/AgentStockage.java
@@ -1,4 +1,14 @@
 package Agents;
 
-public class AgentStockage {
+import jade.core.Agent;
+
+/**
+ * agent chargé de stocker les donnees filtrée et categorisée dans la base de donnee de services
+ */
+public class AgentStockage extends Agent {
+
+    protected void setup() {
+        System.out.println("Agent Stockage : " + getLocalName() + " lancé");
+
+    }
 }
