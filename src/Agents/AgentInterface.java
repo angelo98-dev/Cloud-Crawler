58b49c50a067134215fc7893b355e260949f8123
angelo98-dev <angeyannickmien@gmail.com>
2020-09-06 15:36:08 +0100

    Agent Interface

--- src/Agents/AgentInterface.java
+++ src/Agents/AgentInterface.java
@@ -1,4 +1,14 @@
 package Agents;
 
-public class AgentInterface {
+import jade.core.Agent;
+
+/**
+ * agent jouant le role d'interface entre le systeme crawler et le systeme externe
+ */
+public class AgentInterface extends Agent {
+
+    protected void setup() {
+        System.out.println("Agent Interface : " + getLocalName() + " lancé");
+
+    }
 }
