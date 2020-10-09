872d8a5d44d7734b23a1db743d5b75a96bbc5537
angelo98-dev <angeyannickmien@gmail.com>
2020-10-09 22:28:41 +0100

    Ajout du comportement d'extraction

--- src/Agents/AgentInteraction.java
+++ src/Agents/AgentInteraction.java
@@ -5,7 +5,7 @@ import jade.core.Agent;
 /**
  * agent jouant le role d'interface entre le systeme crawler et le systeme externe
  */
-public class AgentInterface extends Agent {
+public class AgentInteraction extends Agent {
 
     protected void setup() {
         System.out.println("Agent Interface : " + getLocalName() + " lancé");
